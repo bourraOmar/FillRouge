bec676067a8016db19db51aaf7ab375bb49f1491
bourraOmar <bwrtmr@gmail.com>
2025-04-10 16:46:28 +0100

    update the route of login and sign up

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/', function () {
     return view('Home');
 })->name('home');
+
 Route::get('/cataloge', function () {
     return view('Cataloge');
 })->name('cataloge');
+
 Route::get('/detail', function () {
     return view('Detail');
 })->name('detail');
-Route::get('/signup', function (){
+
+Route::get('/signup', function () {
     return view('SignUp');
 })->name('signup');
-Route::get('/login', function (){
+Route::post('/signup', [UserController::class, 'signup'])->name('signup.post');
+
+
+Route::get('/login', function () {
     return view('login');
 })->name('login');
-Route::get('/profile', function (){
-    return view('profile');
-})->name('profile')->middleware('role:client');
-Route::get('/dashboardAdmin', function (){
-    return view('DashboardAdmin');
-})->name('dashboardAdmin')->middleware('role:admin');
-Route::get('/dashboardOwner', function (){
-    return view('DashboardOwner')->middleware('role:owner');
+Route::post('/login', [UserController::class, 'login'])->name('login.post');
+
+Route::get('/profile', function () {
+    $user = Auth::user();
+    return view('Profil',compact('user'));
+})->name('profile');
+
+Route::get('/dashboardOwner', function () {
+    
+    return view('DashboardOwner');
 })->name('dashboardOwner');
+
+Route::post('/register', [UserController::class, 'register'])->name('register');
+
+Route::middleware(['RoleMiddleware'])->prefix('admin')->group(function () {
+
+    Route::get('/dashboardAdmin', function () {
+        return view('DashboardAdmin');
+    })->name('dashboardAdmin');
+
+});
