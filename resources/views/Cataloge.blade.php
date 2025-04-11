d3b215dd8b9b46a948d8a14968fccc9c0b46eba8
bourraOmar <bwrtmr@gmail.com>
2025-04-11 16:45:58 +0100

    apdate the view

--- resources/views/Cataloge.blade.php
+++ resources/views/Cataloge.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Auto Rent Pro</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=account_circle" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50 min-h-screen">
