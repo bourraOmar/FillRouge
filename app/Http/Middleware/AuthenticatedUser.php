ee114d396d27ddd5eb97ae2b469f829b164e21ac
bourraOmar <bwrtmr@gmail.com>
2025-04-11 16:41:34 +0100

    add AuthenticatedUser middleware

--- app/Http/Middleware/AuthenticatedUser.php
+++ app/Http/Middleware/AuthenticatedUser.php
@@ -4,17 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthenticatedUser
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
+        if (Auth::check()) {
+            $user = Auth::user();
+            return match ($user->role) {
+                'admin' => redirect()->route('dashboardAdmin'),
+                'owner' => redirect()->route('dashboardOwner'),
+                'client' => redirect()->route('profile'),
+            };
+        }
         return $next($request);
     }
 }
